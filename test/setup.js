60ed600fd07f9ab5a5d98bb2baa977c4f7b43d33
renatobenks <renatobenkendorfs@gmail.com>
2018-06-13 06:50:22 -0300

    test(core): adjusting tests

---
+++ test/setup.js
@@ -0,0 +1,7 @@
+// import dotEnv from 'dotenv-safe';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// dotEnv.load();
+
+Enzyme.configure({ adapter: new Adapter() });
