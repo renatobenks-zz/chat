053f38d59a164456f0ca0569bd80f1d68c84395a
renatobenks <renatobenkendorfs@gmail.com>
2018-06-13 20:27:35 -0300

    test(app): delete

--- src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import App from '../App';
-
-it('renders without crashing', () => {
-  expect(renderer.create(<App />).toJSON()).toMatchSnapshot();
-});
