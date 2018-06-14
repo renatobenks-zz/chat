c5320d5ce0381da4a16550615aeb84f5e331616e
renatobenks <renatobenkendorfs@gmail.com>
2018-06-14 03:16:12 -0300

    feat(test): enable flow to infer type in mock and mock local storage at window

--- test/__mocks__/document.js
+++ test/__mocks__/document.js
@@ -1,3 +1,5 @@
+// @flow
+
 const document = {
   title: '',
 };
--- test/__mocks__/window.js
+++ test/__mocks__/window.js
@@ -1,7 +1,11 @@
+// @flow
+
 import document from './document';
+import localStorage from './localStorage';
 
 const window = {
   document,
+  localStorage,
 };
 
 global.window = window;
