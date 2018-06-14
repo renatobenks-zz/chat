3d352a3eeef93d2b2d7692db96c569fc3575f6ab
renatobenks <renatobenkendorfs@gmail.com>
2018-06-14 13:37:56 -0300

    refactor(route to path): improve to avoid multiability into finder route

--- src/router/buildPath.js
+++ src/router/buildPath.js
@@ -4,7 +4,7 @@ const PUBLIC_PATH = ((process.env.PUBLIC_PATH: any): string);
 
 const SEPARATOR = '/';
 
-const buildPath = (path: string): string => {
+const buildPath = (path?: ?string): string => {
   if (PUBLIC_PATH.slice(-1) === SEPARATOR && path === SEPARATOR) return PUBLIC_PATH;
   return `${PUBLIC_PATH}${path || ''}`;
 };
--- src/router/routeTo.js
+++ src/router/routeTo.js
@@ -8,21 +8,17 @@ import buildPath from './buildPath';
 
 import type { RouteType, RouteParamsType } from '../TypeDefinition';
 
-const findPathByRouteName = (name: string, routes: Array<RouteType>): ?string =>
-  routes
+const findPathByRouteName = (name: string, routes: Array<RouteType>): ?string => {
+  const [route] = routes
     .map(subroute => {
-      if (subroute.name === name) {
-        return subroute.path;
-      }
-
-      if (subroute.routes) {
-        return findPathByRouteName(name, subroute.routes);
-      }
-
+      if (subroute.name === name) return subroute.path;
+      if (subroute.routes) return findPathByRouteName(name, subroute.routes);
       return null;
     })
-    .filter(subroute => subroute && subroute.length > 0)
-    .shift();
+    .filter(subroute => subroute && subroute.length > 0);
+
+  return route;
+};
 
 const replaceParams = (path: string, params: RouteParamsType): string => {
   return Object.keys(params).reduce((accumulator, currentValue) => {
@@ -32,9 +28,7 @@ const replaceParams = (path: string, params: RouteParamsType): string => {
 
 const routeTo = (name: RouteName, params: RouteParamsType = {}): string => {
   const path = findPathByRouteName(name, allRoutes);
-  if (!path) {
-    return buildPath();
-  }
+  if (!path) return buildPath();
   return replaceParams(path, params);
 };
 
