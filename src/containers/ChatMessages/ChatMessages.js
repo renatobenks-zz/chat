dbbdf68673ba4f32fa28981de7b20994d53b7d66
renatobenks <renatobenkendorfs@gmail.com>
2018-06-15 01:21:33 -0300

    feat(chat messages container): add indicator to loading fetched messages

--- src/containers/ChatMessages/ChatMessages.js
+++ src/containers/ChatMessages/ChatMessages.js
@@ -2,39 +2,61 @@
 
 import * as React from 'react';
 
+import Loading from '../../components/Loading/Loading';
+
+import theme from '../../theme';
+
 import type { Conversation, Messages, User } from '../../data';
 
 type State = {
   messages: ?Messages,
   user: ?User,
+  isLoading: boolean,
 };
 
 type Props = {
-  conversation: Conversation,
+  conversation: ?Conversation,
   children: React.ComponentType<{ messages: ?Messages, user: ?User }>,
 };
 
 class ChatMessages extends React.Component<Props, State> {
+  timeout: TimeoutID;
+
   state = {
     messages: null,
     user: null,
+    isLoading: false,
   };
 
   componentDidMount() {
     this.fetchChatMessages();
   }
 
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
   fetchChatMessages = () => {
-    const { conversation } = this.props;
-    this.setState({
-      messages: conversation.messages,
-      user: conversation.user,
-    });
+    this.setState({ isLoading: true });
+
+    this.timeout = setTimeout(() => {
+      const { messages, user } = this.props.conversation || {};
+
+      this.setState({
+        isLoading: false,
+        messages: messages || null,
+        user: user || null,
+      });
+    }, 1500);
   };
 
   render() {
-    const { messages, user } = this.state;
+    const { messages, user, isLoading } = this.state;
     const { children: Children } = this.props;
+
+    if (isLoading) {
+      return <Loading color={theme.palette.secondary} noBackground />;
+    }
 
     return <Children messages={messages} user={user} />;
   }
