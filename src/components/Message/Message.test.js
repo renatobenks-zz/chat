5da6e801ef15ff16ac6eb2f57e302f3ff5598407
renatobenks <renatobenkendorfs@gmail.com>
2018-06-14 21:37:39 -0300

    test(message): rendering cover

---
+++ src/components/Message/Message.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { render } from '../../../test/helpers';
+
+import Message from './Message';
+
+const renderer = component => render(component, { withTheme: true });
+
+it('message with not children', () => {
+  expect(renderer(<Message />)).toMatchSnapshot();
+});
+
+it('message with children', () => {
+  expect(renderer(<Message>some message</Message>)).toMatchSnapshot();
+});
+
+it('message with label', () => {
+  expect(renderer(<Message label="some label">some message</Message>)).toMatchSnapshot();
+});
+
+it('message from me', () => {
+  expect(renderer(<Message isFromMe={true}>some message</Message>)).toMatchSnapshot();
+});
